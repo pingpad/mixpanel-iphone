5d85a48ef18a8d02572eb52a3a9a7f3db60d45f1
David HM Spector <spector@zeitgeist.com>
2015-02-17 19:50:48 -0800

    - First cut at constructing Mixpanel as a bridged framework for use in Swift Apps

---
+++ Framework/Mixpanel/Mixpanel/Mixpanel.h
@@ -0,0 +1,20 @@
+//
+//  Mixpanel.h
+//  Mixpanel
+//
+//  Created by David HM Spector on 2/17/15.
+//  Copyright (c) 2015 Pingpad Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mixpanel.
+FOUNDATION_EXPORT double MixpanelVersionNumber;
+
+//! Project version string for Mixpanel.
+FOUNDATION_EXPORT const unsigned char MixpanelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mixpanel/PublicHeader.h>
+
+
+#import <Mixpanel/Mixpanel.h>
